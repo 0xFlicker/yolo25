14d61212d69524c32c420bf333a1db4475208731
maxflowo2 <cryptobymaxflowo2@gmail.com>
2025-02-28 13:06:04 -0700

    mint vevault pos added

---
+++ packages/aero/src/interface/IVeVaultDeposit.sol
@@ -0,0 +1,43 @@
+/*     +%%#-                           ##.        =+.    .+#%#+:       *%%#:    .**+-      =+
+ *   .%@@*#*:                          @@: *%-   #%*=  .*@@=.  =%.   .%@@*%*   +@@=+=%   .%##
+ *  .%@@- -=+                         *@% :@@-  #@=#  -@@*     +@-  :@@@: ==* -%%. ***   #@=*
+ *  %@@:  -.*  :.                    +@@-.#@#  =@%#.   :.     -@*  :@@@.  -:# .%. *@#   *@#*
+ * *%@-   +++ +@#.-- .*%*. .#@@*@#  %@@%*#@@: .@@=-.         -%-   #%@:   +*-   =*@*   -@%=:
+ * @@%   =##  +@@#-..%%:%.-@@=-@@+  ..   +@%  #@#*+@:      .*=     @@%   =#*   -*. +#. %@#+*@
+ * @@#  +@*   #@#  +@@. -+@@+#*@% =#:    #@= :@@-.%#      -=.  :   @@# .*@*  =@=  :*@:=@@-:@+
+ * -#%+@#-  :@#@@+%++@*@*:=%+..%%#=      *@  *@++##.    =%@%@%%#-  =#%+@#-   :*+**+=: %%++%*
+ *
+ * @title IVeVaultDeposit
+ * @author @maxflowo2 and, @0xflick on github, MGGA!
+ */
+
+// SPDX-License-Identifier: Apache-2.0
+
+/******************************************************************************
+ * Copyright 2025 VeVault                                                     *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ * You may obtain a copy of the License at                                    *
+ *                                                                            *
+ *     http://www.apache.org/licenses/LICENSE-2.0                             *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ ******************************************************************************/
+
+pragma solidity >=0.7.0 <0.9.0;
+
+interface IVeVaultDeposit {
+    /// @dev this is for deposit to msg.sender
+    /// @param lockedAmount is the value equivalent in $aero
+    function depositLP(uint256 lockedAmount) external;
+
+    /// @dev this is for deposit to anyone
+    /// @param to address (non payable) for recipient
+    /// @param lockedAmount is the value equivalent in $aero
+    function depositLPFor(address to, uint256 lockedAmount) external;
+}
